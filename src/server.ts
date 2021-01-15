97f77d3d27f1972e7789a8b01e1769b34ab67697
Joao Marcos <joaoobx@gmail.com>
2021-01-15 14:37:34 -0300

    ESLint, Prettier, Editorconfig e dotenv

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3333;
+
+app.get('/', (request, response) => {
+    response.json({ message: 'GREENGROCER' });
+});
+
+app.listen(port, () => {
+    console.log(`Greengrocer server has started on port: ${port}`);
+});
+
+// Descobrir como trabalhar com express JSON
+// Dar uma olhada em o que é CORS
